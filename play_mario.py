c936109bd58d06bd853989e52ddc7a4661bf9931
GetmoneyStayshiny <robertsliipac@gmail.com>
2019-10-25 10:54:43 +0200

    final model

--- play_mario.py
+++ play_mario.py
@@ -1,18 +1,21 @@
-import gym
+
 import gym_super_mario_bros
+import gym
 from stable_baselines import PPO2
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 
-env = gym_super_mario_bros.make('SuperMarioBros2-v0')
+
+env = gym_super_mario_bros.make('SuperMarioBros-v0')
+#env = JoypadSpace(env, SIMPLE_MOVEMENT)
 env = DummyVecEnv([lambda: env])
 
-
-model = PPO2.load("ppo_mario.p")
+#model = PPO2(MlpPolicy, env, verbose=1)
+model = PPO2.load("ppo_mario32_2mil (4).p")
 
 # Enjoy trained agent
 obs = env.reset()
-for i in range(1000):
+for i in range(200000):
     action, _states = model.predict(obs)
     obs, rewards, dones, info = env.step(action)
     env.render()
